358f44fdccde2fc9dad4e6b4fb62508bc49746b5
My Nguyen <nguyenmy494041@gmail.com>
2020-08-14 16:49:16 +0700

    nháp

---
+++ test/WBDDemo/WBDDemo/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WBDDemo.Models;
+
+namespace WBDDemo.Controllers
+{
+    public class HomeController:Controller
+    {
+        private IEmployeeRepository employeeRepository;
+        public HomeController()
+        {
+            employeeRepository = new EmployeeRepository();
+        }
+        public ViewResult Index()
+        {
+            //ViewData
+            ViewData["employees"] = employeeRepository.Gets();
+            return View();
+        }
+    }
+}
---
+++ test/WBDDemo/WBDDemo/Models/EmployeeRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WBDDemo.Models
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private List<Employee> employees;
+        public EmployeeRepository()
+        {
+            employees = new List<Employee>()
+            {
+                new Employee (){Id = 1,Fullname = "Chicharito",Email ="hatdaunho@gmail.com",Department="It",AvatarPath="~/images/soaica.jfif"},
+                new Employee (){Id = 2,Fullname = "PaulScholes",Email ="hoangtutocvang@gmail.com",Department="It",AvatarPath="~/images/no_image.jfif"}
+            };
+        }
+        public IEnumerable<Employee> Gets()
+        {
+            return employees;
+        }
+    }
+}
